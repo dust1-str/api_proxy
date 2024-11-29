8e87f82a444d95dcc4f6f276de19f0c38f60906f
Angel T <angel.t069@outlook.com>
2024-11-28 22:39:20 -0600

    Routes added and dev script modified

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,16 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import SongsController from '#controllers/songs_controller'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.get('/songs', [SongsController, 'index'])
+router.post('/songs', [SongsController, 'store'])
+router.put('/songs/:id', [SongsController, 'update'])
+router.delete('/songs/:id', [SongsController, 'destroy'])
+
